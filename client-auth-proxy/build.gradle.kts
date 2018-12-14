f7728ca95e2664bba47e7a682819a6dbeb61d4a4
Weinan Qiu <davidiamyou@gmail.com>
2018-12-14 15:29:04 -0500

    client-auth-proxy module

---
+++ client-auth-proxy/build.gradle.kts
@@ -0,0 +1,57 @@
+import build.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    idea
+    kotlin("jvm")
+    id("org.springframework.boot")
+    id("org.jetbrains.kotlin.plugin.spring")
+    id("io.spring.dependency-management")
+}
+
+group = "io.imulab.astrea.service"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${Version.springCloud}")
+    }
+}
+
+dependencies {
+    implementation(project(":oauth-sdk"))
+    implementation(project(":oidc-sdk"))
+    implementation(project(":client-sdk"))
+    implementation(project(":discovery-sdk"))
+
+    kotlin()
+    grpc()
+    jBCrypt()
+    jose4j()
+    logging()
+
+    springBoot(
+        loadKotlinSupport = true,
+        loadTestSupport = true,
+        loadZuul = true
+    )
+
+    resilience4j(loadRetry = true)
+
+    test(
+        loadSpek2 = true,
+        loadMockitoKotlin = true,
+        loadAssertj = true
+    )
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+    }
+}
