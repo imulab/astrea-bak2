9affb3e35564e5e37f1335792fa9605ac71d9867
Weinan Qiu <davidiamyou@gmail.com>
2018-12-16 09:02:29 -0500

    LoginRedirectionFilter

---
+++ authorize-proxy/src/main/kotlin/io/imulab/astrea/service/proxy/Config.kt
@@ -0,0 +1,6 @@
+package io.imulab.astrea.service.proxy
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ProxyConfiguration
---
+++ authorize-proxy/src/main/kotlin/io/imulab/astrea/service/proxy/login/LoginFilter.kt
@@ -0,0 +1,40 @@
+package io.imulab.astrea.service.proxy.login
+
+import com.netflix.zuul.ZuulFilter
+import com.netflix.zuul.context.RequestContext
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+import java.util.*
+
+abstract class LoginFilter : ZuulFilter() {
+
+    protected companion object {
+        const val LoginApproved = "LOGIN_APPROVED"
+    }
+
+    override fun filterType(): String = FilterConstants.PRE_TYPE
+
+    override fun shouldFilter(): Boolean {
+        val context = RequestContext.getCurrentContext()
+        return !context.getBoolean(LoginApproved, false)
+    }
+
+    protected fun hashRequestQueryParams(
+        entryFilter: (Map.Entry<String, List<String>>) -> Boolean = { true }
+    ): String {
+        val joined = RequestContext.getCurrentContext()
+            .requestQueryParams.toSortedMap()
+            .entries
+            .filter(entryFilter)
+            .joinToString { it.key + ":" + it.value.joinToString() }
+
+        return MessageDigest.getInstance("SHA-256")
+            .digest(joined.toByteArray())
+            .let { Base64.getUrlEncoder().withoutPadding().encodeToString(it) }
+    }
+
+    protected fun setApproved() {
+        RequestContext.getCurrentContext().set(LoginApproved, true)
+    }
+}
