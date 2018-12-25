8f12edec3e46d72fde90dcf92c80ad8d108e5ad5
Weinan Qiu <davidiamyou@gmail.com>
2018-12-25 16:57:01 -0500

    clean up old services

--- authorize-proxy/src/main/kotlin/io/imulab/astrea/service/proxy/Redirection.kt
+++
@@ -1,5 +0,0 @@
-package io.imulab.astrea.service.proxy
-
-import java.lang.RuntimeException
-
-class RedirectionSignal(val status: Int, val url: String) : RuntimeException("Redirecting to $url with status $status.")
